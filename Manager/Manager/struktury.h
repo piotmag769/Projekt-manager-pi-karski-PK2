6d5dea61017daa641538126065d4c4c6dbeec2f7
Piotr Magiera <piotrmagiera150@gmail.com>
2020-07-29 13:37:54 +0200

    2.

--- Manager/Manager/struktury.h
+++ Manager/Manager/struktury.h
@@ -6,5 +6,43 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct lk
+{
+	int nr_id;
+	struct lk* pNext;
+};
+
+struct zawodnik
+{
+	char* imie;
+	char* nazwisko;
+	int umiejetnosci;
+	int kontuzja;
+	int klauzula_odstepnego;
+	int tygodniowka;
+};
+
+struct trener
+{
+	char* imie;
+	char* nazwisko;
+	int umiejetnosci;
+	int klauzula_odstepnego;
+	int tygodniowka;
+};
+
+struct druzyna
+{
+	char* nazwa_druzyny;
+	int nr_id;
+	int points;
+	int budget;
+	int wages;
+	struct zawodnik squad[11];
+	struct zawodnik bench[5];
+	struct zawodnik juniors[5];
+	struct trener trainer;
+	struct druzyna* pNext;
+};
 
 #endif
